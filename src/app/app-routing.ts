ff20d41aec9fd66cf1ee3f5cc04df8132c3af867
Junico Vilela <junico.vilela@gmail.com>
2025-07-15 01:17:56 -0300

    removendo teste categoria, lançamentose relatórios

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -1,33 +1,27 @@
-import { Routes } from "@angular/router";
+import { Routes } from '@angular/router';
 import { AuthGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
-    path: 'auth',
-    loadChildren: () => import('./pages/auth/auth-routes').then(r => r.authRoutes)
-  },
-  {
-    path: 'entries',
-    loadChildren: () => import('./pages/entries/entries-routes').then(m => m.ENTRIES_ROUTES),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'categories',
-    loadChildren: () => import('./pages/categories/categories-routes').then(m => m.CATEGORIES_ROUTES),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'colaboradores',
-    loadChildren: () => import('./pages/colaboradores/colaboradores-routes').then(m => m.COLABORADORES_ROUTES),
-    canActivate: [AuthGuard]
-  },
-  {
-    path: 'reports',
-    loadChildren: () => import('./pages/reports/reports-routes').then(m => m.REPORTS_ROUTES),
-    canActivate: [AuthGuard]
-  },
-  {
     path: '',
-    redirectTo: '/reports', pathMatch: 'full'
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'colaboradores',
+        loadChildren: () => import('./pages/colaboradores/colaboradores-routes').then(m => m.COLABORADORES_ROUTES),
+      },
+      {
+        path: '',
+        redirectTo: '/colaboradores', pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./pages/auth/auth-routes').then(m => m.authRoutes),
+  },
+  {
+    path: '**',
+    redirectTo: '/colaboradores'
   }
 ]; 
