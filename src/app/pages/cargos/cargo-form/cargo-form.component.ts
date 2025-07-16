a70aede5ceeae7ee678b5125751c0d7a0c55e756
Junico Vilela <junico.vilela@gmail.com>
2025-07-16 00:13:27 -0300

    - adicionando departamento ao cargo

--- src/app/pages/cargos/cargo-form/cargo-form.component.ts
+++ src/app/pages/cargos/cargo-form/cargo-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
 import { Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
@@ -6,6 +6,8 @@ import { RouterLink } from '@angular/router';
 import { BaseResourceFormComponent } from '../../../shared/components/base-resource-form/base-resource-form.component';
 import { Cargo, NivelCargo, NivelCargoLabels } from '../shared/cargo.model';
 import { CargoService } from '../shared/cargo.service';
+import { Departamento } from '../../departamentos/shared/departamento.model';
+import { DepartamentoService } from '../../departamentos/shared/departamento.service';
 
 import { PageHeaderComponent } from '../../../shared/components/page-header/page-header.component';
 import { FormFieldErrorComponent } from '../../../shared/components/form-field-error/form-field-error.component';
@@ -28,10 +30,11 @@ import { BreadCrumbComponent } from '../../../shared/components/bread-crumb/brea
         BreadCrumbComponent
     ]
 })
-export class CargoFormComponent extends BaseResourceFormComponent<Cargo> {
+export class CargoFormComponent extends BaseResourceFormComponent<Cargo> implements OnInit {
 
     niveisCargoOptions = Object.values(NivelCargo);
     nivelCargoLabels = NivelCargoLabels;
+    departamentos: Departamento[] = [];
 
     get isEditMode(): boolean {
         return this.resource.id !== null && this.resource.id !== undefined;
@@ -45,9 +48,26 @@ export class CargoFormComponent extends BaseResourceFormComponent<Cargo> {
 
     constructor(
         protected cargoService: CargoService,
+        protected departamentoService: DepartamentoService,
         protected override injector: Injector
     ) {
         super(injector, new Cargo(), cargoService, Cargo.fromJson);
+    }
+
+    override ngOnInit(): void {
+        super.ngOnInit();
+        this.loadDepartamentos();
+    }
+
+    private loadDepartamentos(): void {
+        this.departamentoService.getAll().subscribe({
+            next: (departamentos) => {
+                this.departamentos = departamentos.filter(d => d.ativo);
+            },
+            error: (error) => {
+                console.error('Erro ao carregar departamentos:', error);
+            }
+        });
     }
 
     protected buildResourceForm() {
@@ -56,7 +76,7 @@ export class CargoFormComponent extends BaseResourceFormComponent<Cargo> {
             nome: [null, [Validators.required, Validators.minLength(2), Validators.maxLength(100)]],
             descricao: [null, [Validators.maxLength(250)]],
             nivel: [null, [Validators.required]],
-            departamentoId: [1, [Validators.required]], // Temporário - departamento fixo
+            departamentoId: [null, [Validators.required]],
             salarioBase: [null, [Validators.required, Validators.min(0)]],
             ativo: [true]
         });
