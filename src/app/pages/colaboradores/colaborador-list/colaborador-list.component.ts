5ec14eecac3b483c15877e05d0687b19621ed40d
Junico Vilela <junico.vilela@gmail.com>
2025-07-22 23:37:48 -0300

    - removendo imports

--- src/app/pages/colaboradores/colaborador-list/colaborador-list.component.ts
+++ src/app/pages/colaboradores/colaborador-list/colaborador-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, Injector, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 
